3ac9a6555ce7fd3ebf6224a6101eab7e5331f568
yuvayt <anhtt.swe@gmail.com>
2024-12-23 01:35:53 +0700

    [INIT] tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import deno from '@deno/vite-plugin'
+import { dirname, fromFileUrl, join } from '@std/path'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [deno(), vue()],
+	server: {
+		port: 6789,
+	},
+	resolve: {
+		alias: {
+			'@': join(dirname(fromFileUrl(import.meta.url)), 'src'),
+		},
+	},
 })
