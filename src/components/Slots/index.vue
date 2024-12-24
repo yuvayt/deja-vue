0ec94c50edd29ccaef57df6541730ffb2044d25f
yuvayt <anhtt.swe@gmail.com>
2024-12-25 01:25:09 +0700

    [FEAT] Slot machine

---
+++ src/components/Slots/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import Input from '../common/Input.vue'
+import {ref} from 'vue'
+
+const items = ref<string[]>([])
+
+const newItem = ref("")
+
+const add = () => {
+    if (!newItem.value) return
+
+    items.value.push(newItem.value)
+}
+
+const result = ref<string[]>([]) 
+
+const spin = () => {
+    result.value = ["abc"]
+}
+
+</script>
+
+<template>
+    <div>
+        <div>
+            {{ items.toString() }}
+        </div>
+        <form @submit.prevent="add">
+            <label for="add-item">Item</label>
+            <Input id="add-item" type="text" v-model="newItem"/>
+            <input type="submit" value="Submit">
+        </form>
+       
+        <div>
+            {{ result.toString() }}
+        </div>
+        <button @click="spin">
+            Spin the fkn sht
+        </button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/common/Input.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const model = defineModel();
+</script>
+
+<template>
+  <input
+    class="w-full rounded-md border border-slate-300 outline-none p-3 leading-5 text-gray-900 focus:ring focus:ring-offset-2 focus:ring-offset-slate-50 focus:ring-blue-300"
+    v-model="model"
+  />
+</template>
+
+<style scoped></style>
