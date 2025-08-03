b90fcfb1fef6dfae14772aaaeafbc61299117af7
yuvayt <anhtt.swe@gmail.com>
2025-08-03 17:17:29 +0700

    [DEP] Upgrade tailwindcss to 4.x

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,10 @@
 
 declare module '*.vue' {
 	import type { DefineComponent } from 'vue'
-	const component: DefineComponent<{}, {}, any>
+	const component: DefineComponent<
+		Record<PropertyKey, never>,
+		Record<PropertyKey, never>,
+		unknown
+	>
 	export default component
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import deno from '@deno/vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 import { dirname, fromFileUrl, join } from '@std/path'
 
 // https://vite.dev/config/
 export default defineConfig({
 	base: '/deja-vue/',
-	plugins: [deno(), vue()],
+	plugins: [deno(), vue(), tailwindcss()],
 	server: {
 		port: 6789,
 	},
